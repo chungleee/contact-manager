d0df04dfe5557ccf76bff8a201066863e63698a2
Leon <leon.karho.chung@gmail.com>
2018-11-05 16:42:17 -0500

    Updates

---
+++ precache-manifest.2e8f3ba7749551d42540ad9ded6e1b4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "fef9218a9318d6157686",
+    "url": "/contact-manager/static/js/main.fef9218a.chunk.js"
+  },
+  {
+    "revision": "d3bc909f5ba16d906127",
+    "url": "/contact-manager/static/js/1.d3bc909f.chunk.js"
+  },
+  {
+    "revision": "fef9218a9318d6157686",
+    "url": "/contact-manager/static/css/main.80f2cc21.chunk.css"
+  },
+  {
+    "revision": "d3bc909f5ba16d906127",
+    "url": "/contact-manager/static/css/1.96ed30f1.chunk.css"
+  },
+  {
+    "revision": "a96f74182895db75dc95c07eae783214",
+    "url": "/contact-manager/index.html"
+  }
+];
